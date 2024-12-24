70cd4a6bd7be10c6a507b75952fe088a1910f912
Todd Schulman <toobuntu@gmail.com>
2024-12-23 23:26:09 -0500

    perf(Casks/radiola.rb): simplify livecheck stanza

--- Casks/test-radiola.rb
+++ Casks/test-radiola.rb
@@ -1,4 +1,4 @@
-cask "radiola" do
+cask "test-radiola" do
   version "7.0.2"
   sha256 "834d07f0e55ea874e1270284745123c3d6b312a2618ed5235fb0d5290f8f31e9"
 
@@ -12,21 +12,8 @@ cask "radiola" do
   homepage "https://github.com/SokoloffA/radiola"
 
   livecheck do
-    url :homepage
-    regex(/.*?Radiola[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
-    # Use the github_releases strategy to ignore betas with pre-release tags, and match
-    # the version in the download url because the release tag can be vMAJOR.MINOR
-    # or vMAJOR.MINOR.PATCH but the version is reliably MAJOR.MINOR.PATCH.
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        next if release["draft"] || release["prerelease"]
-
-        curr_asset = release["assets"]&.find { |asset| asset["browser_download_url"]&.match(regex) }
-        next if curr_asset.blank?
-
-        curr_asset["browser_download_url"][regex, 1]
-      end
-    end
+    url :url
+    strategy :github_latest
   end
 
   auto_updates true
